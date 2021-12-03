28c25dc7370d29420d7987d8f2a1b0487e80b6c4
krylowicz <kacper@krylowicz.pl>
2021-12-03 14:42:17 +0100

    finish

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -37,7 +37,19 @@ fig = px.histogram(success_df, x='success', y='campaign', color='campaign')
 st.plotly_chart(fig)
 
 st.subheader('Amount spent by age group')
-
+category = pd.cut(df.Age,bins=[25,40,65,81],labels=['Adults','Seniors', 'Elderly'])
+df.insert(2,'Age_Groups',category)
+mean_spent = df.groupby(['Age_Groups']).Spent.mean().reset_index()
+fig = px.bar(mean_spent, x='Age_Groups', y='Spent', color='Age_Groups')
+fig.update_layout(
+  xaxis=dict(
+    title={'text': ''},
+    tickvals=[0, 1, 2],
+    ticktext=['25-40', '40-65', '65+']
+  ),
+  yaxis={'title': {'text': 'Mean spendings'}}
+)
+st.plotly_chart(fig)
 
 st.subheader('Number of customers per spent group')
 spend_5_500 = df["Spent"][(df["Spent"] >=5) & (df["Spent"] <= 500)]
@@ -101,6 +113,37 @@ and deals has highly significant p-value, hence there is a relationship between 
 
 st.markdown('### Predicting whether a client is a parent based on the amount they spent')
 image = Image.open('src/data/parent_odds.png')
+st.image(image)
+
+st.subheader('Does age vary across customer that accepted the campaing?')
+x = [1, 2, 3, 4, 5, 6]
+y = [0.009691423054819294, 0.8935207294191613, 0.008222422455038243, 0.08503414438616336, 1.1933101456748575e-05, 0.009158292100321172]
+fig = px.scatter(x=x, y=y, color_discrete_sequence=['yellow'])
+fig.update_traces(marker={'size': 16})
+fig.update_layout(
+  xaxis={'title': {'text': 'Number of advertising campaing'}},
+  yaxis={'title': {'text': 'P-value'}}
+)
+fig.add_hrect(y0=1.2, y1=0.05, line_width=0, fillcolor="red", opacity=0.2, annotation_text='rejection region', annotation=dict(font_size=16))
+st.plotly_chart(fig)
+
+x=['1st campaign', '2nd campaign', '3rd campaign', '4th campaign', '5th campaign', '6th campaign' ]
+fig = go.Figure(go.Bar(x=x, y=[8.5, 1.3, 11.1, 5.8, 12.2, 19.8], name='25 - 40'))
+fig.add_trace(go.Bar(x=x, y=[5.3, 1.4, 6.5, 7.2, 5.5, 13.6], name='40 - 65'))
+fig.add_trace(go.Bar(x=x, y=[8.9, 1.1, 6.7, 10,9.4, 15.8], name='Above 65'))
+fig.update_layout(barmode='stack', xaxis={'categoryorder':'array', 'categoryarray':['d','a','c','b']})
+st.plotly_chart(fig)
+st.markdown('''
+We can conclude that the 1st, 3rd , 5th and the last campaign are dependent on the age groups. Althoght we dont know the exact kind of the campaign, statistically we can say that these 4 campaigns especially the last campaign was targeted for specific age group. From the chart below we can conclude that most of the campaign focuses on the age group from 25 to 40 and above 65. the reason could be to motivate this age groups , since most of the customers are not on these two age groups but on the age group 40 to 65. Also we can say that: regardless of the age group , the customer that have accepted the offer in the 2nd campaign is much lower and the customer have accepted the offer in the responce (last campaign) is better than the other. 
+''')
+
+st.markdown('### Spendings after last advertising campaing')
+st.markdown('We used kruskalwallis test to test whether customers that accepted the last camaping spent more. Resulting p-value was equal to 1')
+
+image = Image.open('src/data/box.png')
+st.image(image)
+
+image = Image.open('src/data/camp_spend.png')
 st.image(image)
 
 st.markdown('## 3. Customer clustering')
